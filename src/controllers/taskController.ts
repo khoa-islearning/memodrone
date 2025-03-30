a85a90317312d75daddfb3a2b76686f965db8378
dkhoanguyen1101 <dkhoa.nguyen1101@gmail.com>
2025-03-30 12:58:49 -0600

    update

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,1 +1,9 @@
-export const createTask =
+import { NextFunction } from 'express';
+import Database from 'better-sqlite3';
+
+const db = new Database();
+export const createTask = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {};
